a4731ad840547a0bc7a55781dd7ca269f629f72a
sofisof456 <sofia.blanco12@educa.madrid.org>
2025-05-27 23:40:55 +0200

    funciono!!!

--- GithubRepository/pruebaGithub/src/main/java/newPackage/Main2.java
+++ GithubRepository/pruebaGithub/src/main/java/newPackage/Main2.java
@@ -1,9 +1,9 @@
-package org.example;
+package newPackage;
 
-public class Main {
+public class Main2 {
     public static void main(String[] args) {
 
         System.out.println("Hola, Mundo!");
-        System.out.println("equipo2");
+        System.out.println("repositorio clonado!");
     }
 }
